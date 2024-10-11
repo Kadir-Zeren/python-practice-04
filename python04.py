5297d8a72e90dc0ab828e944a8a089350c88f0d4
kadir-zeren <k4dzeren@gmail.com>
2024-10-12 02:34:32 +0300

    practice

--- python04.py
+++ python04.py
@@ -36,3 +36,148 @@ print(animal[ ::- 1])
 
 vegetable = "Tomato"
 print('length of the word', vegetable, 'is :', len(vegetable))
+
+text = "Clarusway"
+print(text, 'kelimesinin uzunlugu', len(text), 'harften olusur')
+
+str_one = 'upper'
+str_two = 'case'
+str_comb = str_one + str_two
+print('upper' + 'case')
+print(str_one + str_two)
+print(str_comb)
+
+str_one = 'upper'
+str_two = 3 * 'upper'
+str_comb = str_one * 3
+print(str_two)
+print(str_comb)
+print(* str_one)
+print(*text)
+
+string_1 = 'I am angry ... '
+print(* string_1)
+string_2 = '1453'
+print(* string_2)
+'joseph@clarusway . com'
+print(* 'joseph@clarusway.com')
+
+str_one = 'upper'
+str_one += 'case'
+print(str_one)
+str_one += 'letter'
+print(str_one)
+str_one += 'end'
+print(str_one)
+
+a = 5
+a = a + 1
+a += 1
+
+name = 'Yasin'
+print('Merhaba, %s' % name)
+
+name = 'Yasin'
+age = 43
+meslek = 'Content Creator'
+print('Merhaba, ismin %s yasin %d meslegin ise % s' % (name, age, meslek))
+
+name = 'Yasin'
+age = 43.5
+meslek = 'Content Creator'
+print( 'Merhaba, ismin %s yasin %f meslegin ise % s' % (name, age, meslek))
+
+fruit = 'Orange'
+vegetable = 'Tomato'
+amount = 4
+print('The amount of {} we bought is {} pounds' .format(fruit, amount))
+
+name = 'Ali'
+age = 43
+meslek = 'Content Creator'
+
+'Merhaba, ismin {} yasin {} meslegin ise {}'.format(name, age, meslek)
+'Merhaba, ismin {} yasin {} meslegin ise {}'.format(name, age,meslek)
+'Merhaba, ismin {2} yasin {0}meslegin ise {1}'.format(age,meslek, name)
+'Merhaba, ismin {a} yasin {b} meslegin ise {c}' . format(b = age,c =meslek, a = name)
+
+print('{state} is the most {adjective} state of the {country}'.format(state='California',country='USA', adjective='crowded' ))
+
+fruit = 'Orange'
+vegetable = 'Tomato'
+amount = 4
+print('The amount of {} we bought is {} pounds' . format(fruit, amount))
+
+
+print('{0} is the most {adjective} state of the {country}' .format('California',country='USA' ,adjective='crowded' ))
+
+'Merhaba, ismin {} , yasin {b} meslegin ise {c}' . format(name, c = meslek, b = age)
+
+taban = 6
+yukseklik = 4
+'burada hesaplama gosterecegim {}' .format(taban*yukseklik/2)
+
+print("{}-{}-{}".format("12", "Feb", "Feb"))
+print("{no}-{month}-{month}".format(no="12", month="Feb"))
+print("{6} {5} {0} {1} {3} {4} {2}". format("a new", "job", "months", "in", 6, "have started", "I will"))
+
+
+name = 'Ali'
+age = 43
+meslek = 'Content Creator'
+f'merhaba benim adim {name} yasim {age} meslegim {meslek}'
+
+print(f'merhaba benim adim {name} yasim {age} meslegim {meslek}')
+print( 'Merhaba, ismin {} , yasin {b} meslegin ise {c}' . format(name, c = meslek, b = age))
+
+fruit = 'Orange'
+vegetable = 'Tomato'
+amount = 6
+output = f"The amount of {fruit} and {vegetable} we bought are totally {amount} pounds"
+print(output)
+
+sample = f"{2 ** 3}"
+print(sample)
+
+name = "MARIAM"
+
+my_name = 'MARIAM'
+output = f"My name is {my_name.capitalize()}"
+print(output)
+
+name = 'MARIAM'
+print(F'My name is {name. capitalize()}')
+
+f'saga hizalama {name:>20}'
+f'saga hizalama {name:<20}'
+f'saga hizalama {name:^20}'
+f'saga hizalama {name:*^20}'
+
+name = "Joseph"
+job = "teachers"
+domain = "Data Science"
+message = (
+f"Hi {name}. "
+f"You are one of the {job} "
+f"in the {domain} section."
+)
+print(message)
+
+name = "Joseph"
+job = "teachers"
+domain = "Data Science"
+message = f"Hi {name}. " \
+f"You are one of the {job} " \
+f"in the {domain} section."
+print(message)
+
+name = "Susan"
+age = "young"
+gender = "lady"
+school = "CLRWY IT university"
+output = (
+f"{name} is a {age} "
+f" {gender} and she is a student "
+f"at the {school}."
+)
+print(output)
